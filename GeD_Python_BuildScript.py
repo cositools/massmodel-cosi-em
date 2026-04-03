1822947008422e76a01c403941ea59620dadade7
ckierans <kierans.c@gmail.com>
2026-04-03 03:31:43 -0400

    Updated sims files to include Simple detector and GR+3DStrip combined volume

--- GeD_Python_BuildScript.py
+++ GeD_Python_BuildScript.py
@@ -1,10 +1,22 @@
 
 #read in the template for the GeD definition
-f = open("COSISMEX.Ge.DetectorBuild.geo.template","r")
-gedtemplate = f.read()
-f.close()
+f_geo = open("COSISMEX.Ge.DetectorBuild.geo.template","r")
+ged_geo_template = f_geo.read()
+f_geo.close()
+
+f_det_sims = open("COSISMEX.Ge.DetectorBuild.SimsOnly.det.template","r")
+ged_det_sims_template = f_det_sims.read()
+f_det_sims.close()
+
+f_det_anl = open("COSISMEX.Ge.DetectorBuild.Analysis.det.template","r")
+ged_det_anl_template = f_det_anl.read()
+f_det_anl.close()
+
+
+
 
 #Listed for GeD_0 (HP52217-1), GeD_1 (Mirion "good EM1" (M61574))
+#TODO Read parameters from file
 CrystalDiameter_A = [10.10, 9.97]
 HandleSpan_B = [9.6, 9.54]
 DetectorWidthX_C = [8.0, 8.03]
@@ -19,6 +31,7 @@ fnames = []
 detlines = ['']
 triggerlines = [ '' ]
 
+#TODO generalize to 16 detectors to get Q#L# with modulo of 4
 for i,detheight in enumerate(DetectorHeight_E):
 
 	lines = ['Constant CrystalDiameter_Q0_L' + str(i) + ' ' + str(CrystalDiameter_A[i]),\
@@ -31,63 +44,23 @@ for i,detheight in enumerate(DetectorHeight_E):
 			'Constant GuardRingSize_Q0_L' + str(i) + ' ' + str(GuardRingSize),\
 			'']
 
-	fname = 'COSISMEX.Ge.DetectorBuild.Q0L' + str(i) + '.geo'; fnames.append(fname)
+	fname = 'COSISMEX.Ge.Q0L' + str(i) + '.geo'; fnames.append(fname)
 	fout = open(fname,'w')
 	fout.writelines(map(lambda x:x + '\n',lines))
-	fout.write( gedtemplate.replace('QQ_QQ', 'Q0_L'+str(i)) )
+	fout.write( ged_geo_template.replace('QQ_QQ', 'Q0_L'+str(i)) )
+	fout.close()
+
+	fname = 'COSISMEX.Ge.SimsOnly.Q0L' + str(i) + '.det'; fnames.append(fname)
+	fout = open(fname,'w')
+	fout.write( ged_det_sims_template.replace('QQ_QQ', 'Q0_L'+str(i)).replace('QQQQ', 'Q0L'+str(i)).replace('GeD_X', 'GeD_'+str(i)) )
+	fout.close()
+
+	fname = 'COSISMEX.Ge.Analysis.Q0L' + str(i) + '.det'; fnames.append(fname)
+	fout = open(fname,'w')
+	fout.write( ged_det_anl_template.replace('QQ_QQ', 'Q0_L'+str(i)).replace('QQQQ', 'Q0L'+str(i)).replace('GeD_X', 'GeD_'+str(i)) )
 	fout.close()
 
 
-	#add in the detector + trigger info
-#	detector = 'Detector' + str(i)
-#	trigger = detector + '_Trigger'
-#	grtrigger = detector + '_GRTrigger'
-#	detlines = ["Include " + fname + '\n']
-#	detlines += ['Strip3D ' + detector,\
-#				 detector + '.DetectorVolume ' + ged_wafer,\
-#				 detector + '.SensitiveVolume ' + ged_wafer,\
-#				 detector + '.StructuralPitch 0.0 0.0 0.0',\
-#				 detector + '.StructuralOffset 0.0 0.0 0.0',\
-#				 detector + '.Offset 0.32 0.32',\
-#				 detector + '.StripNumber 37 37',\
-#				 detector + '.EnergyResolution Gauss 0 0 1.02',\
-#				 detector + '.EnergyResolution Gauss 100 100 1.02',\
-#				 detector + '.EnergyResolution Gauss 300 300 1.04',\
-#				 detector + '.EnergyResolution Gauss 1000 1000 1.07',\
-#				 detector + '.EnergyResolution Gauss 3000 3000 1.15',\
-#				 detector + '.EnergyResolution Gauss 10000 10000 1.42',\
-#				 detector + '.DepthResolution 100 0.02',\
-#				 detector + '.NoiseThreshold 15.0',\
-#				 detector + '.TriggerThreshold 40.0',\
-#				 detector + '.GuardringEnergyResolution 100 1.5',\
-#				 detector + '.GuardringEnergyResolution 1000 1.5',\
-#				 detector + '.GuardringTriggerThreshold 40',\
-#				 detector + '.FailureRate 0.0',\
-#				 '']
-#	triggerlines = [ 'Trigger ' + trigger,\
-#				 trigger + '.Veto False',\
-#				 trigger + '.TriggerByChannel True',\
-#				 trigger + '.Detector ' + detector + ' 1',\
-#				 '',\
-#				 'Trigger ' + grtrigger,\
-#				 grtrigger + '.Veto True',\
-#				 grtrigger + '.TriggerByDetector True',\
-#				 grtrigger + '.GuardringDetector ' + detector + ' 1',\
-#				 '']
-#
-#	# Write the individual .det files
-#	fname = 'GeD_DetectorBuild_' + str(i) + '.det'
-#	fout = open(fname,'w')
-
-#	fout.writelines(map(lambda x:x + '\n',detlines))
-#	fout.writelines(map(lambda x:x + '\n',triggerlines))
-#	fout.close()
-
-
-
-# Now write the tigger criteria file
-#fout = open('GeD_TriggerCriteria.det','w')
-#fout.writelines(map(lambda x:x + '\n',triggerlines))
-#fout.close()
+print(fnames)
 
 
